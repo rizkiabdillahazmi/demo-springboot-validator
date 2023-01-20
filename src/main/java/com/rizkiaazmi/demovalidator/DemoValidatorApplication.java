793f8bb0ce4e4f73443048d5782816c8b176f5e5
Rizki Abdillah Azmi <rizki.abdillah@mobbi.id>
2023-01-20 15:58:25 +0700

    setup spring boot project

---
+++ src/main/java/com/rizkiaazmi/demovalidator/DemoValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.rizkiaazmi.demovalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoValidatorApplication.class, args);
+    }
+
+}
